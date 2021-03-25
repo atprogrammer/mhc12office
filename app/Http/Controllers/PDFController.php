7167008ef26ecf3f6659bc7985e76dace0fa34b0
atprogrammer <atprogrammer@gmail.com>
2021-03-25 23:03:43 +0700

    250364

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use PDF;
-//use Dompdf\Dompdf;
+use PDF; //use PDF
 
 class PDFController extends Controller
 {
@@ -17,10 +16,32 @@ class PDFController extends Controller
     public function pdf()
     {
        
-       $pdf = PDF::loadView('pdf.testpdf');
-       return @$pdf->stream();
+        // PDF::AddPage('L', 'A4');
        
- 
-       
+        // PDF::SetFont('THSarabunNew', 'B', 20, '', 'false');
+        // PDF::SetY(15);  //ระยะห่างจากด้านบนมาล่าง
+        // PDF::SetX(0);  //ระยะห่างจากซ้ายไปขวา
+        // PDF::Cell(0, 0, 'อีสานเดฟ มหาสารคาม', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+
+        // PDF::SetFont('THSarabunNew', 'B', 16, '', 'false');
+        // PDF::SetY(30);  //ระยะห่างจากด้านบนมาล่าง
+        // PDF::SetX(10);  //ระยะห่างจากซ้ายไปขวา
+        // PDF::Cell(0, 0, 'วัน/เดือน/ปี', 1, false, 'C', 0, '', 0, false, 'M', 'M');
+
+        // PDF::SetFont('THSarabunNew', 'B', 16, '', 'false');
+        // PDF::SetY(34);  //ระยะห่างจากด้านบนมาล่าง
+        // PDF::SetX(10);  //ระยะห่างจากซ้ายไปขวา
+        // PDF::MultiCell(40, 5, 'เดือน/ปี', 1, 'C',0, 1, '', '', true);
+
+        // PDF::Output('PDF-Report.pdf','I');
+      
+        $view = \View::make('pdf.testpdf');
+        $html_content = $view->render();
+        PDF::SetY(10);  //ระยะห่างจากด้านบนมาล่าง
+        PDF::SetFont('THSarabunNew', 'B', 16, '', 'false');
+        PDF::SetTitle("ใบเบิกสื่อ");
+        PDF::AddPage();
+        PDF::writeHTML($html_content, true, false, true, false, '');
+        PDF::Output('userlist.pdf');    
     }
 }
--- resources/views/pdf/testpdf.blade.php
+++ resources/views/pdf/testpdf.blade.php
@@ -1,173 +1,146 @@
-<html>
-<header>
-<title>pdf</title>
-<meta http-equiv="Content-Language" content="th" />
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<style>
-  @font-face{
-   font-family:  'THSarabunNew';
-   font-style: normal;
-   font-weight: normal;
-   src: url("{{asset('fonts/THSarabunNew.ttf') }}") format('truetype');
-  }
-  @font-face{
-   font-family:  'THSarabunNew';
-   font-style: normal;
-   font-weight: bold;
-   src: url("{{asset('fonts/THSarabunNew Bold.ttf') }}") format('truetype');
-  }
-  @font-face{
-   font-family:  'THSarabunNew';
-   font-style: italic;
-   font-weight: normal;
-   src: url("{{asset('fonts/THSarabunNew Italic.ttf') }}") format('truetype');
-  }
-  @font-face{
-   font-family:  'THSarabunNew';
-   font-style: italic;
-   font-weight: bold;
-   src: url("{{asset('fonts/THSarabunNew BoldItalic.ttf') }}") format('truetype');
-  }
-  body{
-   font-family: "THSarabunNew";
-   font-size: 18px;
-  }
-  @page {
-        size: A4;
-        padding: 15px;
-      }
-      @media print {
-        html, body {
-          width: 210mm;
-          height: 297mm;
-          /*font-size : 16px;*/
-        }
-      }
+<div align="center" > <h1>ใบเบิกสื่อ</h1></div>
+  <table border="0" cellpadding="2" width="100%" style="margin-bottom: 100px;">
+  
+         <tr>
+            <td><u>เรื่อง</u> ขอสนับสนุนสื่อเทคโนโลยีสุขภาพจิต</td>
+            <td ></td>
+            <td ></td>
+         </tr>
+          <tr>
+            <td><u>เรียน</u> ผู้อำนวยการศูนย์สุขภาพจิตที่ 12</td>
+            <td ></td>
+            <td ></td>
+          </tr>
+          <tr>
+            <td width="60%">ด้วยฝ่ายบริหารทั่วไป ศูนย์สุขภาพจิตที่ 12</td>
+            <td ></td>
+            <td ></td>
+          </tr>
+          <tr>
+            <td width="100%">มีความประสงค์จะขอรับการสนับสนุนสื่อต่างๆ  เพื่อใช้  ………............................................………………………………………………….</td>
+            <td ></td>
+            <td ></td>
+          </tr>
 
-
-
-#customers {
-  font-family: Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
-
-#customers td, #customers th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
-
-#customers tr:nth-child(even){background-color: #f2f2f2;}
-
-#customers tr:hover {background-color: #ddd;}
-
-#customers th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #4CAF50;
-  color: white;
-}
-
-
-p.dotted {border-style: dotted;}
-p.dashed {border-style: dashed;}
-p.solid {border-style: solid;}
-p.double {border-style: double;}
-p.groove {border-style: groove;}
-p.ridge {border-style: ridge;}
-p.inset {border-style: inset;}
-p.outset {border-style: outset;}
-p.none {border-style: none;}
-p.hidden {border-style: hidden;}
-p.mix {border-style: dotted dashed solid double;}
-  </style>
-</header>
-<body>
-  <img src="https://www.w3schools.com/images/w3schools_green.jpg" alt="W3Schools.com" style="width:104px;height:142px;">
-  <center>
-<h1>Header</h1>
-  </center>
-<br>
-
-<table id="customers">
-  <tr>
-    <th>ไทย</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>ไทย</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Berglunds snabbköp</td>
-    <td>Christina Berglund</td>
-    <td>Sweden</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel</td>
-    <td>Austria</td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Königlich Essen</td>
-    <td>Philip Cramer</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Canada</td>
-  </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Italy</td>
-  </tr>
-  <tr>
-    <td>North/South</td>
-    <td>Simon Crowther</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Paris spécialités</td>
-    <td>Marie Bertrand</td>
-    <td>France</td>
-  </tr>
+         
+        {{-- <img src="{{asset('images/book/title/title.png')}}" alt="" style="width:50px;height:50px;"> --}}
 </table>
 
-<h2>The border-style Property</h2>
-<p>This property specifies what kind of border to display:</p>
-
-<p class="dotted">A dotted border.</p>
-<p class="dashed">A dashed border.</p>
-<p class="solid">A solid border.</p>
-<p class="double">A double border.</p>
-<p class="groove">A groove border.</p>
-<p class="ridge">A ridge border.</p>
-<p class="inset">An inset border.</p>
-<p class="outset">An outset border.</p>
-<p class="none">No border.</p>
-<p class="hidden">A hidden border.</p>
-<p class="mix">A mixed border.</p>
-
-<p>Insert an image from another folder:</p>
-<img src="/images/stickman.gif" alt="Stickman" width="24" height="39">
-
-<p>Insert an image from a web site:</p>
-<img src="https://www.w3schools.com/images/lamp.jpg" alt="Lamp" width="32" height="32">
-
-</body>
-</html>
+  <table border="1" cellpadding="2" width="100%" style="margin-bottom: 100px;">
+      <tr>
+          <th width="10%" align="center">รหัส</th>
+          <th width="75%" align="center">รายการ</th>
+          <th width="15%" align="center">จำนวน</th>
+      </tr>
+    
+           <tr>
+              <td align="center">1</td>
+              <td > แบบประเมินความฉลาดทางอารมณ์เด็กอายุ 6 - 11 ปี สำหรับพ่อแม่/ผู้ปกครอง</td>
+              <td align="center">DDDD เล่ม</td>
+           </tr>
+            <tr>
+              <td align="center">2</td>
+              <td > น้ำDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD</td>
+              <td align="center">DDDD</td>
+            </tr>
+            <tr>
+              <td align="center">44</td>
+              <td > DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD</td>
+              <td align="center">DDDD</td>
+            </tr>
+            <tr>
+              <td align="center">5</td>
+              <td > DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD</td>
+              <td align="center">DDDD</td>
+            </tr>
+            <tr>
+              <td align="center">56</td>
+              <td > DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD</td>
+              <td align="center">DDDD</td>
+            </tr>
+            <tr>
+              <td align="center">สวัสดี</td>
+              <td > DDDDDDDDDDDDDDDDDDDD</td>
+              <td align="center">DDDD</td>
+            </tr>
+            <tr>
+              <td align="center">สวัสดี</td>
+              <td > DDDD</td>
+              <td align="center">DDDD</td>
+            </tr>
+            <tr>
+              <td align="center">สวัสดี</td>
+              <td > DDDD</td>
+              <td align="center">DDDD</td>
+            </tr>
+            <tr>
+              <td align="center">สวัสดี</td>
+              <td > DDDD</td>
+              <td align="center">DDDD</td>
+            </tr>
+            <tr>
+              <td align="center">สวัสดี</td>
+              <td > DDDD</td>
+              <td align="center">DDDD</td>
+            </tr>
+            <br>
+           
+          {{-- <img src="{{asset('images/book/title/title.png')}}" alt="" style="width:50px;height:50px;"> --}}
+  </table>
+<p>
+  <table border="0" cellpadding="2" width="100%" style="margin-bottom: 100px;">
+  
+    <tr>
+       <td></td>
+       <td ></td>
+       <td align="center"><img src="{{asset('images/book/title/title.png')}}" alt="" style="width:60px;height:20px;"> :ผู้ขอเบิก</td>
+    </tr>
+     <tr>
+       <td></td>
+       <td ></td>
+       <td align="center">(นายกนกศักดิ์ ศักดิ์คำแหง)</td>
+     </tr>
+     <tr>
+      <td></td>
+      <td ></td>
+      <td align="center">17/02/2564</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td ></td>
+      <td align="center">ได้รับเอกสารดังกล่าวเรียบร้อยแล้ว</td>
+    </tr>
+    <tr>
+      <td align="center">อนุมัติ</td>
+      <td ></td>
+      <td align="center"><img src="{{asset('images/book/title/title.png')}}" alt="" style="width:50px;height:20px;">:ผู้รับเอกสาร</td>
+    </tr>
+    <tr>
+      <td align="center"><img src="{{asset('images/book/title/title.png')}}" alt="" style="width:50px;height:20px;"></td>
+      <td ></td>
+      <td align="center">(นายวิทยา หาดดี)</td>
+    </tr>
+    <tr>
+      <td align="center">(นางสาวลลิภัทร  บัวทอง)</td>
+      <td ></td>
+      <td align="center">17/02/2564</td>
+    </tr>
+    <tr>
+      <td align="center">นักจิตวิทยาคลินิกชำนาญการ</td>
+      <td ></td>
+      <td align="center"><img src="{{asset('images/book/title/title.png')}}" alt="" style="width:50px;height:20px;">:ผู้จ่าย</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td ></td>
+      <td align="center">(นายกนกศักดิ์  ศักดิ์คำแหง)</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td ></td>
+      <td align="center">17/02/2564</td>
+    </tr>
+    
+    
+   {{-- <img src="{{asset('images/book/title/title.png')}}" alt="" style="width:50px;height:50px;"> --}}
+</table>
